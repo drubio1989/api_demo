c8f9c10722647974b7dc13f8e2e3d6bc9797afab
Daniel Rubio <drubio1989@gmail.com>
2020-07-04 14:10:45 -0500

    Add api pagination and refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,19 @@
 class ArticlesController < ApplicationController
+
   def index
-    @articles = Article.all
-    render json: @articles
+    @articles = Article.all.order(created_at: :desc)
+      .page(params[:page]).per(params[:per_page] || 10)
+    render_serialized_data(@articles)
   end
 
   def show
     @article = Article.find(params[:id])
-    render json: @article
+    render_serialized_data(@article)
+  end
+
+  private
+
+  def render_serialized_data(object)
+    render json: ArticleSerializer.new(object).serializable_hash
   end
 end
--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :request do
+RSpec.describe "ArticlesController", type: :request do
+  describe '#index' do
+    subject { get articles_path }
 
+    it 'returns ok response' do
+      subject
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns proper json' do
+      create_list(:article, 10)
+      subject
+      expect(json_data.length).to eq(10)
+    end
+
+    it 'has correct keys' do
+      create(:article)
+      subject
+      expect(json_data[0].has_key? "id").to  be true
+      expect(json_data[0].has_key? "type").to be true
+      expect(json_data[0]["attributes"].keys).to include( "title", "content", "slug")
+    end
+
+    it 'returns article by most recent' do
+      new_article = create(:article)
+      old_article = create(:article, created_at: DateTime.now - 1.days)
+      subject
+      expect(json_data.first["id"]).to eq(new_article.id.to_s)
+      expect(json_data.last["id"]).to eq(old_article.id.to_s)
+    end
+  end
 end
